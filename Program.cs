edf9e78f22d41b2fa859d9b9deeb82085a83f9ca
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-09-19 16:38:36 +0530

    created a console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Learn c#");
+// Console.WriteLine("Hello, World!");
+// Console.Write("Hello");
+// Console.Write("World");
+Console.WriteLine(10*50);
+
